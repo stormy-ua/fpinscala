71548ecc51af58dddef1aebd38290402957f6d94
Kirill <kirill.panarin@gmail.com>
2016-08-24 20:40:27 -0400

    Option excercises

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,19 +1,38 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Option => _, Some => _, None => _, Either => _, _}
+
+// hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case None => None
+      case Some(v) => Some(f(v))
+    }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+    def getOrElse[B>:A](default: => B): B =
+      this match {
+        case None => default
+        case Some(v) => v
+      }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+    def flatMap[B](f: A => Option[B]): Option[B] =
+      this map f getOrElse None
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+    def orElse[B>:A](ob: => Option[B]): Option[B] =
+      this map (Some(_)) getOrElse ob
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+    def filter(f: A => Boolean): Option[A] =
+      this flatMap (x =>
+        f(x) match {
+          case true => Some(x)
+          case false => None
+        })
 }
+
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
@@ -38,7 +57,9 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap(m => mean(xs map(x => math.pow(x - m, 2))))
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
 
